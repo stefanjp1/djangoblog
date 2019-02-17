5c94a67006092124e1820e0eda3eb3ab265e51ec
Stefan Partin <stefanjp@gmail.com>
2019-02-17 07:40:05 -0800

    added details page

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import stub_view, list_view
+from .views import stub_view, list_view, detail_view
 
 urlpatterns = [
     path('',
         list_view,
         name="blog_index"),
+    path('posts/<int:post_id>/',
+         detail_view, 
+         name="blog_detail"),
     path('posts/<int:post_id>/', stub_view, name="blog_detail"),
 ]
