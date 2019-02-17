fffbbaf94b0612c9c6d9ea013fde234b69fd1923
Stefan Partin <stefanjp@gmail.com>
2019-02-17 08:29:05 -0800

    added categorization to post in admin

--- myblog/admin.py
+++ myblog/admin.py
@@ -4,5 +4,17 @@ from django.contrib import admin
 from myblog.models import Category
 from myblog.models import Post
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+    exclude = ('posts',)
+    
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
